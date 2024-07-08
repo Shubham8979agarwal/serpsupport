d2d391fb12aeaf64575ca02781082ef2ac31fa46
Shubham8979agarwal <agarwalshubham214@gmail.com>
2024-07-08 16:54:03 +0530

    'save'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,35 +30,3 @@ class AppServiceProvider extends ServiceProvider
         }
     }
 }
-<?php
-
-namespace App\Providers;
-
-#use Illuminate\Routing\UrlGenerator;
-use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-
-    public function boot()
-    {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
-    }
-}
