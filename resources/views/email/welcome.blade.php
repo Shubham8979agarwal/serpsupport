75ed160069f0d85bad7e13ecb8da41c16c79e426
Shubham8979agarwal <agarwalshubham214@gmail.com>
2024-07-08 16:59:14 +0530

    'save'

--- resources/views/email/welcome.blade.php
+++ resources/views/email/welcome.blade.php
@@ -9,8 +9,6 @@ body {
 <center>        
 <h1>Welcome to SerpSupport</h1>
 <h3>SerpSupport welcomes you {{$name}}</h3>
-<!-- <p>As you know you signup with google on SerpSupport and we do not save anyone's google password for privacy concerns therefore your password is your username which is: {{$username}} in our database followed by @serpsuppport. So your password will be {{$username}}@serpsupport</p>
-<p>You can reset your password from here: https://serp.curv.store/forget-password </p> -->
 </center>
 </body>
 </html>
