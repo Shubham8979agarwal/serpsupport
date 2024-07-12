04c9007a66b83cd4f631f074ace1a06c934a19e8
Shubham8979agarwal <agarwalshubham214@gmail.com>
2024-07-12 19:42:41 +0530

    'save'

--- database/migrations/2024_06_21_112022_create_websites_table.php
+++ database/migrations/2024_06_21_112022_create_websites_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('websites', function (Blueprint $table) {
             $table->id();
-            $table->string('user_id');
-            $table->string('website_id');
-            $table->string('website_niche');
-            $table->string('website_url');
-            $table->longText('website_description');
-            $table->string('website_uploader_email');
+            $table->string('user_id')->nullable();
+            $table->string('website_id')->nullable();
+            $table->string('website_niche')->nullable();
+            $table->string('website_url')->nullable();
+            $table->longText('website_description')->nullable();
+            $table->string('website_uploader_email')->nullable();
             //$table->rememberToken();
             $table->timestamps();
         });
